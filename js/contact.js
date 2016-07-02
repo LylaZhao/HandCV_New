f362daa49fdd12b37d53dbd9655562ad0a740a47
Lyla Zhao <zhaoyulyla@gmail.com>
2016-07-02 18:22:45 +0800

    first version

--- js/contact.js
+++ js/contact.js
@@ -4,8 +4,9 @@ $(function () {
 
     $('#contact-form').on('submit', function (e) {
         if (!e.isDefaultPrevented()) {
+            /*var url = "./contact.php";*/
             var url = "contact.php";
-
+            
             $.ajax({
                 type: "POST",
                 url: url,
@@ -13,11 +14,14 @@ $(function () {
                 success: function (data)
                 {
                     var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
+                    var messageText = "您的留言已发送。Your message has been sent.";//data.message;
 
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
+                    
+                    
+                    if (1 ||  messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
+                        //alert(alertBox+"\n"+$('#contact-form').find('.messages'));
                         $('#contact-form')[0].reset();
                     }
                 }
